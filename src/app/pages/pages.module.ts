d832c55668c59ff984a28d2dbca325625da8e340
Karoline de Souza Quetz <kquetz@viceri.com.br>
2023-08-03 13:57:14 -0300

    feat: created header

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { RoutingModule } from './routing.module';
 import { DetailsComponent } from './details/details.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [HomeComponent, DetailsComponent],
-  imports: [CommonModule, RoutingModule],
+  imports: [CommonModule, RoutingModule, SharedModule],
 })
 export class PagesModule {}
