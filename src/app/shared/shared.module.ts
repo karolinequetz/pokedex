9fa5284fd8dcb3785b0199512d40bd011653f9d8
Karoline de Souza Quetz <kquetz@viceri.com.br>
2023-08-04 14:06:45 -0300

    feat: route created for details

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,10 +5,11 @@ import { CommonModule } from '@angular/common';
 import { PokeHeaderComponent } from './poke-header/poke-header.component';
 import { PokeSearchComponent } from './poke-search/poke-search.component';
 import { PokeListComponent } from './poke-list/poke-list.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [PokeHeaderComponent, PokeSearchComponent, PokeListComponent],
   exports: [PokeHeaderComponent, PokeSearchComponent, PokeListComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
 })
 export class SharedModule {}
