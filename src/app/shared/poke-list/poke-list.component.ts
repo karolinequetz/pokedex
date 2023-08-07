c771f5b8512af28c6c766273b9b2963c299fe36f
Karoline de Souza Quetz <kquetz@viceri.com.br>
2023-08-07 16:13:21 -0300

    feat: created details

--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -11,6 +11,8 @@ export class PokeListComponent implements OnInit {
   private setAllPokemons: Pokemon[] = [];
   public getAllPokemons: Pokemon[] = [];
 
+  public apiError: boolean = false;
+
   constructor(private pokeApiService: PokeApiService) {}
 
   ngOnInit(): void {
@@ -18,7 +20,10 @@ export class PokeListComponent implements OnInit {
       next: (res) => {
         this.setAllPokemons = res.results;
         this.getAllPokemons = this.setAllPokemons;
-        console.log(this.getAllPokemons);
+        this.apiError = true;
+      },
+      error: (error) => {
+        this.apiError = true;
       },
     });
   }
